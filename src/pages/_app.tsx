21de6f649146b46cf7a699493d992ffa2ceadfeb
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-14 05:33:10 -0300

    feat: add stitches and add logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,23 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Roboto } from "@next/font/google";
+import { globalStyles } from "@/styles/global";
+
+import logoImg from "@/assets/logo.svg";
+import Image from "next/image";
+import { Container, Header } from "@/styles/pages/app";
+
+const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
+
+globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Container className={roboto.className}>
+      <Header>
+        <Image src={logoImg.src} alt="" width={120} height={40} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
