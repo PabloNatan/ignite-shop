d463f32ae092c114ab47cdfed9fd079a495149a6
Pablo Santos <pablo.1999@alunos.utfpr.edu.br>
2023-02-16 07:13:01 -0300

    feat: integration success page

--- src/pages/sucess.tsx
+++ src/pages/sucess.tsx
@@ -1,21 +1,64 @@
+import { stripe } from "@/lib/stripe";
 import { ImageContainer } from "@/styles/pages/product";
 import { SuccessContainer } from "@/styles/pages/success";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 import Link from "next/link";
+import Stripe from "stripe";
 
-export default function Success() {
+interface SuccessProps {
+  customerName: string;
+  product: {
+    name: string;
+    imageUrl: string;
+  };
+}
+
+export default function Success({ customerName, product }: SuccessProps) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada</h1>
 
-      <ImageContainer></ImageContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} height={120} width={110} alt="" />
+      </ImageContainer>
 
       <p>
-        Uhuul <strong>Diego Fernandes</strong>, sua{" "}
-        <strong>Camiseta Beyond the Limits</strong> já está a caminho da sua
-        casa.
+        Uhuul <strong>{customerName}</strong>, sua{" "}
+        <strong>{product.name}</strong> já está a caminho da sua casa.
       </p>
 
       <Link href="/">Voltar para o catálogo</Link>
     </SuccessContainer>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  if (!query.session_id) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  const sessionId = String(query.session_id);
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ["line_items", "line_items.data.price.product"],
+  });
+
+  const customerName = session.customer_details?.name;
+  const product = session.line_items?.data[0].price?.product as Stripe.Product;
+
+  return {
+    props: {
+      customerName,
+      product: {
+        name: product.name,
+        imageUrl: product.images[0],
+      },
+    },
+  };
+};
